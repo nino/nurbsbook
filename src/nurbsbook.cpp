9d6555fad4b63f5744b6ec59df3ee753d74b4cce
Nino Annighoefer <nino@ninoan.com>
2024-09-10 19:43:12 +0100

    Use views instead of normal for loop

--- src/nurbsbook.cpp
+++ src/nurbsbook.cpp
@@ -1,4 +1,5 @@
 #include "nurbsbook.hpp"
+#include <ranges>
 
 namespace nurbsbook {
 
@@ -6,15 +7,13 @@ int twice(int num) { return num * 2; }
 
 double eval_polynomial(std::span<double> coefficients, double x) {
   auto result = 0.0;
-  for (auto i = coefficients.size(); i > 0; i -= 1) {
-    result = result * x + coefficients[i - 1];
+  for (auto coeff : coefficients | std::views::reverse) {
+    result = result * x + coeff;
   }
   return result;
 }
 
-double bernstein(int i, int n, double u) {
-
-}
+double bernstein(int i, int n, double u) {}
 
 Eigen::Vector3d stretch(Eigen::Vector3d input) { return input * 3.3; }
 
