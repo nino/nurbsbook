32129ca1dbb632bb42c2e625dc1adff809105c43
Nino Annighoefer <nino@ninoan.com>
2024-09-10 19:53:23 +0100

    More tests for eval_polynomial

--- test/nurbsbook_test.cpp
+++ test/nurbsbook_test.cpp
@@ -11,3 +11,16 @@ TEST(NurbsBookTest, Polynomial1) {
   auto actual = nurbsbook::eval_polynomial(coeffs, 0);
   EXPECT_EQ(actual, 0);
 }
+
+TEST(NurbsBookTest, Polynomial2) {
+  using nurbsbook::eval_polynomial;
+
+  std::vector<double> coeffs = {3.2, 6, 8.2, -5.2, 3.0};
+  auto epsilon = 0.000001;
+  EXPECT_NEAR(eval_polynomial(coeffs, 0), 3.2, epsilon);
+  EXPECT_NEAR(eval_polynomial(coeffs, 0.1), 3.8771, epsilon);
+  EXPECT_NEAR(eval_polynomial(coeffs, 0.25), 5.14296875, epsilon);
+  EXPECT_NEAR(eval_polynomial(coeffs, 10.25), 28440.76796875, epsilon);
+  EXPECT_NEAR(eval_polynomial(coeffs, 2.1), 62.1491, epsilon);
+  EXPECT_NEAR(eval_polynomial(coeffs, 212.1), 6022085007.109099, epsilon);
+}
